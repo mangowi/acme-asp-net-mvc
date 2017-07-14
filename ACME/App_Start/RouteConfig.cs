512d3ee0a28a70883103c41d663973ed3a9254d2
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-15 01:43:00 +0200

    Adding namespaces to RouteConfig

--- ACME/App_Start/RouteConfig.cs
+++ ACME/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace ACME
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] {"ACME.Controllers"}
             );
         }
     }
